b1c6b78e6783c1120885577a47ba0140a264c1c1
Vinibilk <you@example.com>
2021-08-23 15:10:38 -0300

    Tokens And Fun :)

---
+++ Aula65/src/index.js
@@ -0,0 +1,77 @@
+require("dotenv").config()
+const express = require("express");
+const jwt = require("jsonwebtoken")
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+const { User } = require("./models");
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Olá mundo!");
+});
+
+app.post("/login", async (req, res) => {
+
+    try {
+
+        const { email, password } = req.body;
+
+        const user = await User.findOne({
+            where: {
+                email
+            }
+        })
+
+        if (!user) {
+            return res.status(400).json({ message: "E-mail or Password Incorrect!" });
+        }
+
+        if (!user.verifyPassword(password)) {
+            return res.status(400).json({ message: "E-mail or Password Incorrect!" });
+        };
+
+        //Emitindo o acces token
+        const token = jwt.sign({
+            sub: user.id
+        }, process.env.TOKEN_SECRET, {
+            expiresIn: '20s'
+        });
+
+        res.json({ token })
+
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({
+            message: "Oops! Something bad happend"
+        })
+    }
+})
+
+app.post("/users", async (req, res) => {
+    try {
+        const { name, email, password } = req.body;
+
+        const [user, created] = await User.findOrCreate({
+            where: {
+                email
+            },
+            deafaults: {
+                name,
+                password
+            }
+        });
+
+        if (!created) {
+            res.status(409).json({ message: "E-mail already exists" })
+        }
+
+        res.status(201).json(user);
+    } catch (err) {
+        console.log(err);
+        res.status(400).json({ message: "Failed!" });
+    }
+});
+
+app.listen(PORT, () => console.log("Servidor rodando na porta: " + PORT));
